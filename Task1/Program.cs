8963699ee1a174a4d139271b4e2429ed8ad314ec
Olga Charushnikova <olga_charushnikova@bk.ru>
2023-04-25 10:50:10 +0300

    Задача 1

---
+++ Task1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.WriteLine("Введите трехзначное число: ");
+double chislo = Convert.ToInt32(Console.ReadLine());
+double x = chislo/10; 
+double x1 = Math.Floor(x); 
+x = x/10; 
+x = Math.Floor(x);
+int i=0;
+while (x < x1)
+{
+   x = x + 1;
+   i++;
+}
+Console.WriteLine($"{chislo} - > {i}");
